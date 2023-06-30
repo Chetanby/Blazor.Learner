1f148823a22f8739962a54f067307aec65016bd8
Chetan B Y <chetu.by@gmail.com>
2023-06-30 18:58:28 +0530

    Add project files.

---
+++ Blazor.Learner/Server/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using Blazor.Learner.Server.Data;
+using Blazor.Learner.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Blazor.Learner.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+
+        public UserController(ApplicationDBContext context)
+        {
+            this._context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(a => a.Id == id);
+            return Ok(user);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post(User user)
+        {
+            _context.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok(user.Id);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(User user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var user = new User { Id = id };
+            _context.Remove(user);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
